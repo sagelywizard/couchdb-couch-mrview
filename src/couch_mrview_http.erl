269febfc0f3d91e68621213670f2295ab0ea648a
benoitc <bchesneau@gmail.com>
2014-02-07 15:38:34 +0100

    add supports of view changes in the _changes API
    Now when the option `seq_indexed=true` is set in the design doc, the
    view filter in _changes will use it to retrieve the results. Compared to
    the current way, using a view index will be faster to retrieve changes.
    It also gives the possibility to filter changes by key or get changes in
    a key range. All the view options can be used.
    
    Note 1: if someone is trying to filter a changes with view options when
    the views are not indexed by sequence, a 400 error will be returned.
    Note 2: The changes will only be returned when the view is updated if
    seq_indexed=true
    
    Conflicts:
    	src/couch_mrview_http.erl

--- src/couch_mrview_http.erl
+++ src/couch_mrview_http.erl
@@ -34,6 +34,10 @@
     check_view_etag/3
 ]).
 
+-export([parse_boolean/1,
+         parse_int/1,
+         parse_pos_int/1]).
+
 -include_lib("couch/include/couch_db.hrl").
 -include_lib("couch_mrview/include/couch_mrview.hrl").
 
@@ -434,8 +438,10 @@ parse_boolean(true) ->
     true;
 parse_boolean(false) ->
     false;
+
 parse_boolean(Val) when is_binary(Val) ->
     parse_boolean(?b2l(Val));
+
 parse_boolean(Val) ->
     case string:to_lower(Val) of
     "true" -> true;
@@ -444,7 +450,6 @@ parse_boolean(Val) ->
         Msg = io_lib:format("Invalid boolean parameter: ~p", [Val]),
         throw({query_parse_error, ?l2b(Msg)})
     end.
-
 
 parse_int(Val) when is_integer(Val) ->
     Val;
@@ -456,7 +461,6 @@ parse_int(Val) ->
         Msg = io_lib:format("Invalid value for integer: ~p", [Val]),
         throw({query_parse_error, ?l2b(Msg)})
     end.
-
 
 parse_pos_int(Val) ->
     case parse_int(Val) of
